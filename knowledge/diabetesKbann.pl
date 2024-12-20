4bd269b2421f828c2c577c5962c848ba6af502bc
NunzioDA <nunziodamore@gmail.com>
2024-12-20 12:33:36 +0100

    Fixed KBANN training procedure

---
+++ knowledge/diabetesKbann.pl
@@ -0,0 +1,5 @@
+'Outcome'(Age, BMI, BloodPressure, DiabetesPedigreeFunction, Glucose, Insulin, Pregnancies, SkinThickness, HighBMI, LowBMI, HighGlucose, LowGlucose, 1) :-
+    HighGlucose, HighBMI.
+'Outcome'(Age, BMI, BloodPressure, DiabetesPedigreeFunction, Glucose, Insulin, Pregnancies, SkinThickness, HighBMI, LowBMI, HighGlucose, LowGlucose, 0) :-
+    LowBMI, LowGlucose.
+
